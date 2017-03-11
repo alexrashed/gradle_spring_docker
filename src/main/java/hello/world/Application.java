52e33499c0289bfda26863311303047c3bfdd22f
Alexander Rashed <alexander.rashed@gmail.com>
2017-03-11 23:58:26 +0100

    Test continuous deployment

--- src/main/java/hello/world/Application.java
+++ src/main/java/hello/world/Application.java
@@ -11,7 +11,7 @@ public class Application {
 
     @RequestMapping("/")
     public String home() {
-        return "Hello Spring Boot, Docker, Gralde";
+        return "Hello Continuous Deployment";
     }
 
     public static void main(String[] args) {
